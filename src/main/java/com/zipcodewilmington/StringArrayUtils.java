853703ac8c9077dd41e54eb84fe642f6750bec87
Freddy <freddy@Zipcoders-MacBook-Pro-7.local>
2022-11-03 13:23:15 -0400

    Checkpoint2

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington;
 
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Created by leon on 1/29/18.
@@ -84,45 +85,55 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        //instance a String variable combine
-        String combine = "";
+        String result = Arrays.toString(array).toLowerCase();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-        //create a loop for array[] parameter
-        for (int i = 0; i < array.length; i++) {
-
-            //store & combine the array into combine, lowercase it, remove empty space
-            combine+=array[i].toLowerCase().replaceAll(" ","");
-        }
-
-        //split combine into array[] actual by single letters
-        String[] actual = combine.split("");
-
-        //sort actual alphabetically
-        Arrays.sort(actual);
-
-        //create ArrayList<String> called master for array[] actual
-        ArrayList<String> master = new ArrayList<>(List.of(actual));
-
-        //create a loop for ArrayList<>
-        for (int i = 0; i < master.size()-1; i++) {
-
-            //if the next element of master array index is equal, remove element
-            while (master.get(i).equals(master.get(i+1))) {
-                master.remove(i);
+        for (int i = 0; i < alphabet.length(); i++) {
+            if (!result.contains(String.valueOf(alphabet.charAt(i)))) {
+                return false;
             }
         }
-
-        //create ArrayList for alphabet using List.of(array[]) with split into single letters
-        ArrayList<String> alphabet = new ArrayList<>(List.of("abcdefghijklmnopqrstuvwxyz".split("")));
-
-        //if master equals to alphabet, return true
-        if (master.equals(alphabet)) {
-            return true;
-        }
-
-        //otherwise, false
-        return false;
+        return true;
     }
+
+//        //instance a String variable combine
+//        String combine = "";
+//
+//        //create a loop for array[] parameter
+//        for (int i = 0; i < array.length; i++) {
+//
+//            //store & combine the array into combine, lowercase it, remove empty space
+//            combine+=array[i].toLowerCase().replaceAll(" ","");
+//        }
+//
+//        //split combine into array[] actual by single letters
+//        String[] actual = combine.split("");
+//
+//        //sort actual alphabetically
+//        Arrays.sort(actual);
+//
+//        //create ArrayList<String> called master for array[] actual
+//        ArrayList<String> master = new ArrayList<>(List.of(actual));
+//
+//        //create a loop for ArrayList<>
+//        for (int i = 0; i < master.size()-1; i++) {
+//
+//            //if the next element of master array index is equal, remove element
+//            while (master.get(i).equals(master.get(i+1))) {
+//                master.remove(i);
+//            }
+//        }
+//
+//        //create ArrayList for alphabet using List.of(array[]) with split into single letters
+//        ArrayList<String> alphabet = new ArrayList<>(List.of("abcdefghijklmnopqrstuvwxyz".split("")));
+//
+//        //if master equals to alphabet, return true
+//        if (master.equals(alphabet)) {
+//            return true;
+//        }
+//
+//        //otherwise, false
+//        return false;
 
     /**
      * @param array array of String objects
@@ -130,7 +141,13 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].contains(value)) {
+                result++;
+            }
+        }
+        return result;
     }
 
     /**
@@ -139,7 +156,13 @@ public class StringArrayUtils {
      * @return array with identical contents excluding values of `value`
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+        ArrayList<String> lists = new ArrayList<>(List.of(array));
+        for (int i = 0; i < lists.size()-1; i++) {
+            lists.remove(valueToRemove);
+        }
+        System.out.println(lists);
+        String[] result = lists.toArray(new String[0]);
+        return result;
     }
 
     /**
@@ -147,7 +170,15 @@ public class StringArrayUtils {
      * @return array of Strings with consecutive duplicates removes
      */ // TODO
     public static String[] removeConsecutiveDuplicates(String[] array) {
-        return null;
+        ArrayList<String> lists = new ArrayList<>(List.of(array));
+        for (int i = 0; i < lists.size()-1; i++) {
+            if (lists.get(i).equals(lists.get(i+1))) {
+                lists.remove(i+1);
+                i--;
+            }
+        }
+        String[] result = lists.toArray(new String[0]);
+        return result;
     }
 
     /**
@@ -155,7 +186,18 @@ public class StringArrayUtils {
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
-        return null;
+        ArrayList<String> lists = new ArrayList<>(List.of(array));
+        for (int i = 0; i < lists.size()-1; i++) {
+                while (lists.get(i).contains(lists.get(i + 1))) {
+                    lists.set(i+1, lists.get(i) + lists.get(i + 1));
+                    lists.remove(i);
+                }
+            System.out.println(lists);
+        }
+
+        System.out.println(lists);
+        String[] result = lists.toArray(new String[0]);
+        return result;
     }
 
 
